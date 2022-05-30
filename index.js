b823bd0cd392b5bc1dc6a60532321b680147df09
Dashiell Boyles <dashiellboyles@hotmail.com>
2022-05-30 18:05:25 +0100

    fetched data from GH API and found the most used language of a users repos

---
+++ index.js
@@ -0,0 +1,14 @@
+const getLanguage = async(user) => {
+  const userRepops = await fetch(`https://api.github.com/users/${user}/repos`)
+  const dataRepo = await userRepops.json()
+
+  const arrayOfLanguages = dataRepo.map((repo) => {
+    return repo.language
+  })
+
+  const favouriteLanguage  = arrayOfLanguages.sort((a, b) => 
+  arrayOfLanguages.filter(v => v===a).length 
+  - arrayOfLanguages.filter(v => v===b).length).pop()
+
+  return favouriteLanguage
+}
